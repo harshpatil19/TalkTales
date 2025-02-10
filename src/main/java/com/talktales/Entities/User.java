a065b95c32030f92f0341cbe91f0e160d9ad293f
Harsh <patil.harsh1920@gmail.com>
2025-02-10 18:24:04 +0530

    BlogApplication8

--- src/main/java/com/talktales/Entities/User.java
+++ src/main/java/com/talktales/Entities/User.java
@@ -12,6 +12,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -38,4 +41,8 @@ public class User {
 	
 	@OneToMany(mappedBy="user",cascade = CascadeType.ALL)
 	private Set<Comment> comment=new HashSet<>();
+	
+	@ManyToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+	@JoinTable(name="user_role", joinColumns = @JoinColumn(name="user",referencedColumnName = "userid"), inverseJoinColumns = @JoinColumn(name="role",referencedColumnName = "id"))
+	private Set<Role>roles= new HashSet<>();
 }
